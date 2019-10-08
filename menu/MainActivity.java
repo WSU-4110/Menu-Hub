c12bea5ff9da4afff1729c09f853ee43cb037c10
mdislam1 <55167653+mdislam1@users.noreply.github.com>
2019-10-08 19:05:28 -0400

    Add files via upload

---
+++ menu/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.menu;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    //inflating the menu option ot the main activity
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    //open activity from menu click
+    @Override
+    public boolean onOptionsItemSelected( MenuItem item) {
+        int id = item.getItemId();
+        //add homeClass
+        if (id == R.id.home) {
+            Intent intent = new Intent(MainActivity.this, homeClass.class);
+            this.startActivity(intent);
+            return true;
+        }
+        //add searchClass
+        if (id == R.id.search) {
+            Intent intent = new Intent(MainActivity.this, searchClass.class);
+            this.startActivity(intent);
+            return true;
+        }
+        // add settingActivityClass
+        if (id == R.id.setting) {
+            Intent intent = new Intent(MainActivity.this, settingActivity.class);
+            this.startActivity(intent);
+            return true;
+        }
+        //add uploadClass
+        if (id == R.id.upload) {
+            Intent intent = new Intent(MainActivity.this, uploadClass.class);
+            this.startActivity(intent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
