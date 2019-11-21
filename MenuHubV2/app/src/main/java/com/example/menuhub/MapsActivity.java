7b999295c37e885c6b04f409743b2cdd84a8d088
rjaii <gi6195@wayne.edu>
2019-11-21 04:31:59 -0500

    adding user location

--- MenuHubV2/app/src/main/java/com/example/menuhub/MapsActivity.java
+++ MenuHubV2/app/src/main/java/com/example/menuhub/MapsActivity.java
@@ -1,9 +1,12 @@
 package com.example.menuhub;
 
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
-
 import androidx.fragment.app.FragmentActivity;
-
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -11,9 +14,12 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, LocationListener {
 
     private GoogleMap mMap;
+    private LocationManager locationManager;
+    private static final long MIN_TIME = 100;
+    private static final long MIN_DIST = 1000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,16 +29,39 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, MIN_TIME, MIN_DIST, this);
     }
 
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        mMap.setMyLocationEnabled(true);
+    }
 
-        // Add a marker in Detroit and move the camera
-        LatLng detroit = new LatLng(42.3314, -83.0485);
-        mMap.addMarker(new MarkerOptions().position(detroit).title("Marker in Detroit"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(detroit));
+    @Override
+    public void onLocationChanged(Location location) {
+        LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(latLng,10);
+        mMap.animateCamera(cameraUpdate);
+        locationManager.removeUpdates(this);
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
     }
 }
