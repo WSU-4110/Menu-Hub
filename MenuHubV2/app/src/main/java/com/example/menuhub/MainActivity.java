5633049809f25a2537b0d2ef7dee2ed43f67f1f5
AustinAlexand25 <gi5269@wayne.edu>
2019-11-20 19:44:33 -0500

    Cleanup/Almost Final Push

--- MenuHubV2/app/src/main/java/com/example/menuhub/MainActivity.java
+++ MenuHubV2/app/src/main/java/com/example/menuhub/MainActivity.java
@@ -12,6 +12,7 @@ import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.content.Intent;
 import android.os.Bundle;
@@ -28,6 +29,9 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
+    private Button button;
+    private Button button2;
+    private Button button3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +39,28 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         TextView title = (TextView) findViewById(R.id.homeTitle1);
         title.setText("Menu Hub");
+
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openLogin();
+            }
+        });
+        button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openReport();
+            }
+        });
+        button3 = (Button) findViewById(R.id.button3);
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openMaps();
+            }
+        });
 
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -55,9 +81,22 @@ public class MainActivity extends AppCompatActivity {
                         Intent c = new Intent(MainActivity.this,SettingsActivity.class);
                         startActivity(c);
                         break;
+
                 }
                 return false;
             }
         });
+    }
+    public void openLogin() {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
+    public void openReport() {
+        Intent intent = new Intent(this, Reporting.class);
+        startActivity(intent);
+    }
+    public void openMaps() {
+        Intent intent = new Intent(this, MapsActivity.class);
+        startActivity(intent);
     }
 }
---
+++ MenuHubV2/app/src/main/java/com/example/menuhub/User.java
@@ -0,0 +1,13 @@
+package com.example.menuhub;
+
+
+public class User {
+
+    public int id;
+    public String username;
+    public String userpwd;
+
+
+
+
+}
