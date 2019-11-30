4e0c4d929c6a5c128557011ba79ea9237a5f111e
rjaii <gi6195@wayne.edu>
2019-11-30 18:42:24 -0500

    Test

---
+++ MenuHubV2/app/src/test/java/com/example/menuhub/MapsActivityTest.java
@@ -0,0 +1,28 @@
+package com.example.menuhub;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MapsActivityTest {
+
+    @Test
+    public void onCreate() {
+    }
+
+    @Test
+    public void onRequestPermissionsResult() {
+    }
+
+    @Test
+    public void onMapReady() {
+    }
+
+    @Test
+    public void onConnected() {
+    }
+
+    @Test
+    public void checkLocationPermission() {
+    }
+}
